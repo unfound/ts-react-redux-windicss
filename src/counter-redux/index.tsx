8a40afd7f574a023d91de09e801a25ddd7dfc196
unfound <448217185@qq.com>
2021-07-21 16:45:06 +0800

    结合react-redux

--- src/counter-redux/index.tsx
+++ src/counter-redux/index.tsx
@@ -1,10 +1,13 @@
+import { connect } from 'react-redux'
+import { Dispatch } from "redux"
+
 interface CounterReduxProps {
     value: number,
     onIncrement: () => void,
     onDecrement: () => void
 }
 
-export default function CounterRedux ({value, onIncrement, onDecrement}: CounterReduxProps): JSX.Element {
+function CounterRedux ({value, onIncrement, onDecrement}: CounterReduxProps): JSX.Element {
     return (
         <div>
             <h1>{ value }</h1>
@@ -13,3 +16,20 @@ export default function CounterRedux ({value, onIncrement, onDecrement}: Counter
         </div>
     )
 }
+
+function mapStateToProps (state: Record<string, unknown>) {
+    return { value: state.changeCount as number }
+}
+
+function mapDispatchToProps (dispatch: Dispatch) {
+    return {
+        onIncrement: () => {
+            dispatch({ type: 'INCREMENT' })
+        },
+        onDecrement: () => {
+            dispatch({ type: 'DECREMENT' })
+        }
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CounterRedux)
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,24 @@
 import { StrictMode } from "react";
 import { render } from "react-dom";
+import { createStore, combineReducers, applyMiddleware } from 'redux'
+import { Provider } from 'react-redux'
+import thunk, { ThunkMiddleware } from 'redux-thunk'
+import { createLogger } from 'redux-logger'
+import * as reducer from './counter-redux/reducer'
+import { postsBySubreddit } from './utils/redux-fetch'
 import App from "./App";
 import "virtual:windi.css";
 
+const store = createStore(
+  combineReducers({...reducer, postsBySubreddit}),
+  applyMiddleware(thunk as ThunkMiddleware<Record<string, unknown>>, createLogger())
+)
+
 render(
   <StrictMode>
-    <App />
+    <Provider store={ store }>
+      <App />
+    </Provider>
   </StrictMode>,
   document.getElementById("root")
 );
